55879c21502212dd679182be56d02357e39cde9e
Berdia <berdia@Berdias-MacBook-Pro.local>
2025-09-11 19:25:24 +0400

    commit

--- my students/dachi/main.py
+++ my students/dachi/main.py
@@ -1,1 +1,12 @@
-print("dachi")
+# print("dachi")
+
+# print(1 + 1) # 2
+# print("1" + "1") # 11
+# print("1" + 1) # Error
+
+# String - ტექსტი ბრჭყალებში ჩასმული
+# Integer - რიცხვი
+
+# print(2 + 3)
+# print("2" + "3")
+# print("2" + 3)

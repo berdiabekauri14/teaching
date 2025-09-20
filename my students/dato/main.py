56481a6a48772e2461c01daf35ed57ef3d2f9947
Berdia <berdia@Berdias-MBP.lan>
2025-09-20 19:14:52 +0400

    commit

--- my students/dato/main.py
+++ my students/dato/main.py
@@ -134,3 +134,16 @@
 # # snake case and camel case together
 # My_city = "city"
 
+# "" - ბრჭყალი/ბრჭყალები
+# () - ფრჩხილი/ფრჩხილები
+
+# name = "Alice"
+# age = 15
+# city = "Paris"
+# hobby = "painting"
+
+# print("Hello, my name is " + name + "!")
+# print("I am " + str(age) + "years old.")
+# print("I live in " + city)
+# print("My favorite hobby is " + hobby)
+# print("Nice to meet you!")

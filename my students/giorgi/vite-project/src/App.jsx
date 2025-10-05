86e39f5dd0d5e08c350913e58254e21ce132ce34
Berdia <berdia@Berdias-MacBook-Pro.local>
2025-10-05 16:28:57 +0400

    commit

--- my students/giorgi/vite-project/src/App.jsx
+++ my students/giorgi/vite-project/src/App.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+
 export default function App() {
-  
+    const [count, setCount] = useState(0)
+
+    const increase = () => {
+        setCount(count + 1)
+    }
+
+    const decrease = () => {
+        setCount(count - 1)
+    }
+
+    return (
+        <div className=" flex justify-center items-center h-100">
+            <h1>{count}</h1>
+            <br />
+            <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
+        </div>
+    )
 }

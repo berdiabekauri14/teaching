82014a6064c2422a817f1a00b0c4ded6ea33ecb4
Berdia <berdia@Berdias-MacBook-Pro.local>
2025-12-11 19:21:35 +0400

    commit

--- my students/vazha/main.py
+++ my students/vazha/main.py
@@ -6,3 +6,15 @@
 # while i <= 100: 
 #     print(i)
 #     i += 3
+
+# დაწერეთ კოდი, რომელიც შეამოწმებს რომ მომხმარებლის შემოტანილი რიცხვი დადებითია თუ უარყოფითი, სხვა შემთხვევაში თუ მომხმარებელმა შემოიტანა 0 გამოუტანოთ "none"
+
+# num = int(input("Enter a number: "))
+
+# if num > 0:
+#     print("positive")
+# elif num == 0:
+#     print("none")
+# else:
+#     print("negative")
+
